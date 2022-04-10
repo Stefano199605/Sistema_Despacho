a31d3d35f4db443123e2bede6a8b2557f38fc2c5
Stefano199605 <kevin.saldana1996@gmail.com>
2022-04-10 17:08:46 -0500

    creando la clase de capa de negocio
    creando metodo insertar, editar,eliminar

---
+++ Solution1/CapaNegocio/CN_Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+namespace CapaNegocio
+{
+   public class CN_Cliente
+    {
+        private CD_Clientes objectoCD = new CD_Clientes();
+
+        public DataTable MostrarProducto()
+        {
+            DataTable tabla = new DataTable();
+            tabla = objectoCD.Mostrar();
+            return tabla;
+        }
+
+        public void InsertarClient(string dni,string nombre,string apellido,string email,string telefono)
+        {
+            objectoCD.Insertar(Convert.ToChar(dni), nombre, apellido, email, Convert.ToInt32(telefono));
+        }
+        public void EditarClient(string dni,string nombre,string apellido,string email,string telefono)
+        {
+            objectoCD.Editar(Convert.ToChar(dni), nombre, apellido, email, Convert.ToInt32(telefono));
+        }
+        public void EliminarClient(string dni)
+        {
+            objectoCD.Eliminar(Convert.ToChar(dni));
+        }
+
+    }
+}
