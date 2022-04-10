941422ded6a016ba75d5f50e16fd633bf307262e
Stefano199605 <kevin.saldana1996@gmail.com>
2022-04-10 12:25:36 -0500

    cambios en el login

--- Solution1/CapaPresentacion/Login.cs
+++ Solution1/CapaPresentacion/Login.cs
@@ -7,28 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using CapaDatos;
-using Oracle.ManagedDataAccess;
-using Oracle.ManagedDataAccess.Client;
+
+
 namespace CapaPresentacion
 {
     public partial class Login : Form
     {
-        Conexion CON = new Conexion();
+        
 
         public Login()
         {
             InitializeComponent();
         }
 
-        private void btnConectar_Click(object sender, EventArgs e)
-        {
-            CON.AbrirConexion();
-        }
-
-        private void btnDesconectar_Click(object sender, EventArgs e)
-        {
-            CON.CerrarConexion();
-        }
+        
     }
 }
