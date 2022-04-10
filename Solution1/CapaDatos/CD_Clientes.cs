21ce24d0c28e3648b5092a4f351a330191174d88
Stefano199605 <kevin.saldana1996@gmail.com>
2022-04-10 14:12:34 -0500

    llamando procedimientos almacenados

---
+++ Solution1/CapaDatos/CD_Clientes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess;
+using Oracle.ManagedDataAccess.Client;
+
+namespace CapaDatos
+{
+   public class CD_Clientes
+    {
+        private Conexion con = new Conexion();
+        OracleDataReader leer;
+        DataTable tabla = new DataTable();
+        OracleCommand comando = new OracleCommand();
+
+        public DataTable Mostrar()
+        {
+            comando.Connection = con.AbrirConexion();
+            comando = new OracleCommand("SP_SELECT_CLIENTE");
+            comando.CommandType = CommandType.StoredProcedure;
+
+        }
+    }
+}
