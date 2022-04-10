307ad5425f24eed6990dee700ea60e064e98c4a9
Stefano199605 <kevin.saldana1996@gmail.com>
2022-04-10 12:23:29 -0500

    cambios

---
+++ Solution1/CapaDatos/Conexion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess;
+using Oracle.ManagedDataAccess.Client;
+
+namespace CapaDatos
+{
+   public class Conexion
+    {
+        private OracleConnection con = new OracleConnection("Data source=orcl;Password=123;USER ID=proy");
+        public OracleConnection AbrirConexion()
+        {
+            if(con.State== ConnectionState.Closed)
+            {
+                con.Open();
+                
+            }
+            return con;
+            
+        }
+        public OracleConnection CerrarConexion()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            return con;
+        }
+    }
+}
